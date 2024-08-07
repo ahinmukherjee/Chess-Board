4b28d188d4d56527e3954ab70ba48cdb4a5cc29f
ahin mukherjee <mukherjeeahin47@gmail.com>
2024-08-07 20:56:25 +0530

    add new js file

--- click_information.js
+++ click_information.js
@@ -1,15 +1,37 @@
  // clicked aneyone squers then show me her perticular information
 
- const allSquers = document.getElementsByClassName("box-chess")
+ const allSquers = document.getElementsByClassName("box-chess");
  //console.log((allSquers));
+
+ const clickedElements= [];
 
  for( const i of allSquers){
     i.addEventListener('click',function() {
         //console.log(i);
-        const idOfElement = i.getAttribute('id');
-        //console.log(idOfElement);
-        console.log(document.getElementById(idOfElement).innerHTML);
+    // push elements in array
 
-        
+       //  clickedElements.push(i);
+       console.log(clickedElements); 
+       
+       const idOfElement = i.getAttribute("id");
+       console.log(idOfElement);
+       const innerHTMLOfElement= document.getElementById(idOfElement).innerHTML;
+      // console.log(innerHTMLOfElement);
+      //console.log(i);
+
+       if(clickedElements.length > 0){
+
+          clickedElements[0].removeAttribute("style");
+          clickedElements.length= 0;
+       }
+       console.log(i);
+      
+        if (innerHTMLOfElement.includes("black")||
+            innerHTMLOfElement.includes("white")) {
+
+           document.getElementById(idOfElement).style.backgroundColor ="yellow";
+           clickedElements.push(i);
+           
+        }
     });
  }
