77740a9a555e31b48d1d550cf15469d5afd22f1a
ahin mukherjee <mukherjeeahin47@gmail.com>
2024-08-19 19:01:00 +0530

    ab

--- click_information.js
+++ click_information.js
@@ -34,12 +34,5 @@
            
         }
     });
-         // 
-       
-   //       if (innerHTMLOfElement.includes("black")||
-   //       innerHTMLOfElement.includes("white")) {
-
-   //      document.getElementById(idOfElement).style.backgroundColor ="yellow";
-   //      clickedElements.push(i);     
-   //   }
+        
  }
